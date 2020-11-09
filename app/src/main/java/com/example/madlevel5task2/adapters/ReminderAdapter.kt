2af1cfc88fb67a4bfc681ea09f0d091a19ba230a
Sven van Putten <svenvanputten@gmail.com>
2020-11-09 17:32:39 +0100

    Created an adapter and item layout for games

---
+++ app/src/main/java/com/example/madlevel5task2/adapters/ReminderAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.madlevel5task2.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel5task2.R
+import com.example.madlevel5task2.models.Game
+import kotlinx.android.synthetic.main.item_game.view.*
+
+class GameBacklogAdapter(private val games: List<Game>) : RecyclerView.Adapter<GameBacklogAdapter.ViewHolder>(){
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun databind(game: Game) {
+            itemView.tvGameTitle.text = game.title
+            itemView.tvGamePlatform.text = game.platform
+            itemView.tvReleaseDate.text = game.releaseDate.toString()
+        }
+    }
+
+    /**
+     * Creates and returns a ViewHolder object, inflating a standard layout called simple_list_item_1.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_game, parent, false)
+        )
+    }
+
+    /**
+     * Returns the size of the list
+     */
+    override fun getItemCount(): Int {
+        return games.size
+    }
+
+    /**
+     * Called by RecyclerView to display the data at the specified position.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.databind(games[position])
+    }
+}
